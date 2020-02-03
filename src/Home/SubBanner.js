b3b1d4badebf67082481f32cb2bc35335eabfdc2
Homegirl7417 <kj3477417@gmail.com>
2020-02-03 12:22:00 +0900

    home scss & SubBanner.js update

--- src/Home/SubBanner.js
+++ src/Home/SubBanner.js
@@ -4,13 +4,21 @@ import "./scss/SubBanner.scss";
 
 function SubBanner() {
     return (
-        <div className="sub-banner">
-            <img
-                className="sub-banner__image"
-                src="/images/sub-banner.svg"
-                alt="sub banner"
-            ></img>
-        </div>
+            <div className="sub-banner">
+                <div className="sub-banner__imagebox">
+                    <img className="sub-banner__image" src="/images/sub-banner.png"></img>
+                </div>
+                <div className="sub-banner__textbox">
+                    <h2 className="sub-banner__title">
+                        EmoticBox는 모든 플랫폼이<br></br>
+                        쓸 수 있는 이모티콘 SDK입니다.
+                    </h2>
+                    <p className="sub-banner__paragraph">
+                        EmoticStudio에 등록된 이모티콘은 EmoticBox를 통해<br></br>
+                        여러 플랫폼 유저들에게 사용 됩니다.
+                    </p>
+                </div>
+            </div>  
     );
 }
 

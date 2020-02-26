0238dbab4bb887f6eff59198ad0b0d99bfd6881e
Homegirl7417 <kj3477417@gmail.com>
2020-02-27 02:07:17 +0900

    Updates

---
+++ precache-manifest.1a0805f339967c59a339e588a063737c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c282594a108a0b4481fabba931184ee4",
+    "url": "/emotic-studio-frontend/index.html"
+  },
+  {
+    "revision": "0bab7b7e3992b62e0456",
+    "url": "/emotic-studio-frontend/static/css/main.6c01b044.chunk.css"
+  },
+  {
+    "revision": "1490afdee503cf8e3ef2",
+    "url": "/emotic-studio-frontend/static/js/2.8e18eed5.chunk.js"
+  },
+  {
+    "revision": "0bab7b7e3992b62e0456",
+    "url": "/emotic-studio-frontend/static/js/main.9c5ea85b.chunk.js"
+  },
+  {
+    "revision": "1beacbd185d5f23e31f2",
+    "url": "/emotic-studio-frontend/static/js/runtime-main.35111598.js"
+  }
+]);
